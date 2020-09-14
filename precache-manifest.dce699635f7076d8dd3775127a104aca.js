b174a338c5349381e8a18df025ec65cffcc737e0
Ambar Bose <ambar.bs@gmail.com>
2020-09-14 17:13:29 +1000

    Updates

---
+++ precache-manifest.dce699635f7076d8dd3775127a104aca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3bf4723ea28133b8c6b0dcbab8d1e0f8",
+    "url": "/memory-game/index.html"
+  },
+  {
+    "revision": "299339456536f33821b2",
+    "url": "/memory-game/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "97180a8afb40a953695a",
+    "url": "/memory-game/static/js/2.10bcf3c7.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "/memory-game/static/js/2.10bcf3c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "299339456536f33821b2",
+    "url": "/memory-game/static/js/main.45477a7c.chunk.js"
+  },
+  {
+    "revision": "edbde91f6abb6e69834c",
+    "url": "/memory-game/static/js/runtime-main.a1f811c6.js"
+  }
+]);
--- src/store/constants.js
+++
@@ -1,9 +0,0 @@
-export const actionType = {
-  UPDATE_OPENED_CARDS: 'UPDATE_OPENED_CARDS',
-  CLOSE_IMAGE_WITH_INDEX: 'CLOSE_IMAGE_WITH_INDEX',
-  RESET_STATE: 'RESET_STATE',
-  GAME_OVER: 'GAME_OVER',
-  FLIP_ALL: 'FLIP_ALL',
-};
-
-export const gameDimension = { row: 2, col: 5 };
