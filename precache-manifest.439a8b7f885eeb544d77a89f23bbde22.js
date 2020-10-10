a19d9652863e4a699d1305d9beb66aa2ebb3d8e8
Ambar Bose <ambar.bs@gmail.com>
2020-10-10 23:10:47 +1100

    Updates

---
+++ precache-manifest.439a8b7f885eeb544d77a89f23bbde22.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5508888d6805df35bc5351be428cea4",
+    "url": "/memory-game/index.html"
+  },
+  {
+    "revision": "4480883ba3e183f70e8a",
+    "url": "/memory-game/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "fae5de8a034994db9a78",
+    "url": "/memory-game/static/js/2.62aceeeb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/memory-game/static/js/2.62aceeeb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4480883ba3e183f70e8a",
+    "url": "/memory-game/static/js/main.604b9c2b.chunk.js"
+  },
+  {
+    "revision": "edbde91f6abb6e69834c",
+    "url": "/memory-game/static/js/runtime-main.a1f811c6.js"
+  }
+]);
